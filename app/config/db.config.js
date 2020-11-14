4916d722a4f780cdfe95c3ce2b0177beb585ae0b
thanoue <khoikhaguitar.vl@gmail.com>
2020-11-15 03:20:45 +0700

    turn to local db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,22 +1,8 @@
-module.exports = {
-    HOST: "us-cdbr-east-02.cleardb.com",
-    USER: "bc5b03d7601dda",
-    PASSWORD: "de67d8b3",
-    DB: "heroku_4c375c28c6d7b66",
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-};
-
 // module.exports = {
-//     HOST: "localhost",
-//     USER: "root",
-//     PASSWORD: "root",
-//     DB: "test_db",
+//     HOST: "us-cdbr-east-02.cleardb.com",
+//     USER: "bc5b03d7601dda",
+//     PASSWORD: "de67d8b3",
+//     DB: "heroku_4c375c28c6d7b66",
 //     dialect: "mysql",
 //     pool: {
 //         max: 5,
@@ -25,3 +11,17 @@ module.exports = {
 //         idle: 10000
 //     }
 // };
+
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "root",
+    DB: "test_db",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
