2709050c6c9d48e99101e9dc4b31b316e716c130
thanoue <khoikhaguitar.vl@gmail.com>
2021-01-16 14:00:16 +0700

    chang db connection

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,41 +1,41 @@
-// const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'development';
 
-// module.exports = env == 'development' ? {
-//     HOST: "localhost",
-//     USER: "root",
-//     PASSWORD: "root",
-//     DB: "test_db",
-//     dialect: "mysql",
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
-// } : {
-//         HOST: "us-cdbr-east-02.cleardb.com",
-//         USER: "bc5b03d7601dda",
-//         PASSWORD: "de67d8b3",
-//         DB: "heroku_4c375c28c6d7b66",
-//         dialect: "mysql",
-//         pool: {
-//             max: 5,
-//             min: 0,
-//             acquire: 30000,
-//             idle: 10000
-//         }
-//     };
-
-module.exports = {
+module.exports = env == 'development' ? {
     HOST: "localhost",
-    USER: "khoikha",
-    PASSWORD: "123456a@A",
-    DB: "florid_db",
-    dialect: "mysql",   
+    USER: "root",
+    PASSWORD: "root",
+    DB: "test_db",
+    dialect: "mysql",
     pool: {
         max: 5,
         min: 0,
         acquire: 30000,
         idle: 10000
     }
-}
+} : {
+        HOST: "us-cdbr-east-02.cleardb.com",
+        USER: "bc5b03d7601dda",
+        PASSWORD: "de67d8b3",
+        DB: "heroku_4c375c28c6d7b66",
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    };
+
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "khoikha",
+//     PASSWORD: "123456a@A",
+//     DB: "florid_db",
+//     dialect: "mysql",   
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// }
