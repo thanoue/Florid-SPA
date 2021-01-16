4ef5ee547aabac4d23bc12e5c032f8025fbecceb
thanoue <khoikhaguitar.vl@gmail.com>
2021-01-16 15:02:39 +0700

    update env

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,41 +1,51 @@
-// const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'development';
 
-// module.exports = env == 'development' ? {
-//     HOST: "localhost",
-//     USER: "root",
-//     PASSWORD: "root",
-//     DB: "test_db",
-//     dialect: "mysql",
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
-// } : {
-//         HOST: "us-cdbr-east-02.cleardb.com",
-//         USER: "bc5b03d7601dda",
-//         PASSWORD: "de67d8b3",
-//         DB: "heroku_4c375c28c6d7b66",
-//         dialect: "mysql",
-//         pool: {
-//             max: 5,
-//             min: 0,
-//             acquire: 30000,
-//             idle: 10000
-//         }
-//     };
+let dbConfig = {
 
-module.exports = {
-    HOST: "localhost",
-    USER: "khoikha",
-    PASSWORD: "123456a@A",
-    DB: "florid_1",
-    dialect: "mysql",   
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
+};
+
+if(env == 'development'){
+    dbConfig =  {
+            HOST: "localhost",
+            USER: "root",
+            PASSWORD: "root",
+            DB: "test_db",
+            dialect: "mysql",
+            pool: {
+                max: 5,
+                min: 0,
+                acquire: 30000,
+                idle: 10000
+            }
     }
+}else if(env == 'vps'){
+    dbConfig = {
+        HOST: "localhost",
+        USER: "root",
+        PASSWORD: "root",
+        DB: "test_db",
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    }
+}else{
+    dbConfig =  {
+                HOST: "us-cdbr-east-02.cleardb.com",
+                USER: "bc5b03d7601dda",
+                PASSWORD: "de67d8b3",
+                DB: "heroku_4c375c28c6d7b66",
+                dialect: "mysql",
+                pool: {
+                    max: 5,
+                    min: 0,
+                    acquire: 30000,
+                    idle: 10000
+                }
+            };
 }
+
+module.exports = dbConfig;
