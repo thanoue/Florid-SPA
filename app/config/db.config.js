522367509baeb686d2be8c80a4870da002abe975
thanoue <khoikhaguitar.vl@gmail.com>
2021-01-17 12:33:38 +0700

    change mysql user

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,8 +2,8 @@ const env = process.env.NODE_ENV || 'development';
 
 module.exports = env !== 'development' ? {
     HOST: "localhost",
-    USER: "florid",
-    PASSWORD: "123456a@A",
+    USER: "vpsuser",
+    PASSWORD: "123456789",
     DB: "florid_main",
     dialect: "mysql",
     pool: {
