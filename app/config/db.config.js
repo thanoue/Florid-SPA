051640dab025eb94f0929ec97002290d3191c956
thanoue <2khoikhaguitar.vl@gmail.com>
2021-03-18 23:49:46 +0700

    fix db name

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -14,9 +14,9 @@ module.exports = env !== 'development' ? {
     }
 } : {
         HOST: "localhost",
-        USER: "root",
-        PASSWORD: "123456a@A",
-        DB: "florid_local",
+        USER: "vpsuser",
+        PASSWORD: "123456789",
+        DB: "florid_main_test",
         dialect: "mysql",
         pool: {
             max: 5,
